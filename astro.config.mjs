fc344efc48638291da4c5ce33734b448010c70ff
Daniel Sepúlveda <dansepdev@gmail.com>
2025-04-30 16:44:41 -0400

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ['imagekit.io']
-  }
+  },
+  adapter: cloudflare(),
+  output: 'server'
 })
